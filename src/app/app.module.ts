d985d765c536933588ee168274306cad9677125a
Riste Mingov <riste.mingov@gmail.com>
2017-01-04 17:12:05 +0100

    create new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipiesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
